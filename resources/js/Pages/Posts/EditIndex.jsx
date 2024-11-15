5d466da3c905d4d816ea2180a75aae75e08c52db
junsan14 <junsanjunsan14@gmail.com>
2024-11-15 10:18:58 +0200

    win1115

--- resources/js/Pages/Posts/EditIndex.jsx
+++ resources/js/Pages/Posts/EditIndex.jsx
@@ -2,58 +2,26 @@ import parse from 'html-react-parser';
 import { usePage, Link, Head, router, useRemember } from '@inertiajs/react'
 import React, {useEffect, useState } from "react";
 import $ from 'jquery';
+import Authenticated from '@/Layouts/AuthenticatedLayout';
+import {CommonBlog } from './Index';
 
 
-import BlogIndex,{ ShowCategoryTab, setCategoryName } from './Index';
-
-function OldBlog({auth}) {
-    fixedSearch();
-
-    const loadPosts = usePage().props.loadPosts.data;
-    const uri = usePage().component;
-    const [posts, setPosts] = useState(loadPosts);
-    const [keyword, setKeyword] = useState("");
-    const [category, setCategory] = useState("");
-    console.log(uri)
-    const [searchState, setSearchState] = useRemember({
-        keyword:"",
-        category:"",
-    })
-
-    useEffect(() => { 
-        setPosts(loadPosts);
-        if(searchState.category && searchState.keyword){  
-            loadPosts = loadPosts.filter(post => post.category === searchState.category);
-            let reg = new RegExp(searchState.keyword,"gi");
-            setPosts(loadPosts.filter(post =>  String(post['keywords']).match(reg) ));
-            
-        }else if(searchState.category){
-            setPosts(loadPosts.filter(post => post.category === searchState.category));
-
-        }else if(searchState.keyword){
-            let reg = new RegExp(searchState.keyword,"gi");
-            setPosts(loadPosts.filter(post =>  String(post['keywords']).match(reg) ));
-           
-        }else if(!searchState.keyword || !searchState.category){
-            setPosts(loadPosts);
-        }
-        
-      }, [searchState.keyword,searchState.category,loadPosts]);
-    
-     const handleClickVisible = (e,is_show)=>{
+export default function AdminBlog(){
+    const handleClickVisible = (e,is_show)=>{
         let id = e.currentTarget.id;
         $(e.currentTarget).parent().prop('disabled', true);
         $(e.currentTarget).css('cursor', "not-allowed");
-        //console.log(router)
         router.patch(
-            route('page.visible', {id:id, is_show:Number(is_show)},),
-                 {  
-                },
-                 { preserveScroll: true,},
-           
+            route('page.visible', {id:id, is_show:Number(is_show)},
+
+            )
         )
+
+
+
+
      }
-     const handleClickDelete = (e)=>{
+     const handleClickDelete = (e, uri)=>{
         let id = e.currentTarget.id;
         $(e.currentTarget).parent().prop('disabled', true);
         $(e.currentTarget).css('cursor', "not-allowed");
@@ -70,175 +38,20 @@ function OldBlog({auth}) {
                 },
                 preserveScroll: true 
             },
-        
         )
-       
      }
-     const reset = ()=>{
-        $(".search_area_input").val("");
-        setSearchState(searchState => ({...searchState, "keyword":""}))
-        setSearchState(searchState => ({...searchState, "category":""}))
-         setPosts(loadPosts);
-         $(".js-search_area_icon").removeClass("fixed");
-     }
-     const categoryName = [
-        "",
-        "IT",
-        "",
-        "",
-        "LIFE",
-        "JOCV"
-      ]
-     const RendarVisibility= (props)=>{
-        const is_show = props.props[0];
-        const id = props.props[1];
-
-        if(is_show){
-            return(
-                <AiOutlineEye className='icon' id={id} alt='表示する' onClick={(e)=>handleClickVisible(e,1)}/>
-            )
-        }else{
-            return(
-                <AiOutlineEyeInvisible className='icon' id={id} alt='非表示にする' onClick={(e)=>handleClickVisible(e,0)}/>
-            )
-        }
-        
-     }
-     const RendarallPage = ()=>{
-        if(posts.length>0){
-            return(
-                <>
-                {posts.map(({id, title, content,excerpt,thumbnail,created_at,updated_at,published_at,is_show} )=> {
-                    const UpdateDate = ()=>{
-                        if(formatDate(published_at) == '1970/01/01'){
-                          return(
-                            <>
-                                <p className="article_link_remarks_date">
-                                    not published yet
-                                </p>
-                            </>   
-                          )
-                        }else if(formatDate(published_at) == formatDate(updated_at)){
-                            return(
-                                <p className="article_link_remarks_date">
-                                    <MdAccessTime />
-                                    {formatDate(published_at)}
-                                </p>
-                            )
-                        }else{
-                            return(
-                                <>
-                                    <p className="article_link_remarks_date">
-                                        <MdUpdate />
-                                        {formatDate(updated_at)}
-                                    </p>
-                                    <p className="article_link_remarks_date">
-                                        <MdAccessTime />
-                                        {formatDate(published_at)}
-                                    </p>
-                                </>   
-                              ) 
-                        }
-                      }
-                    return(
-                        <div className={is_show?'article edit': 'article edit unshown'} id={id} key={id}>
-                            <div className='id'>
-                                {id}
-                            </div>
-                            <div className="remarks">
-                                <h3 className="remarks_title">{title}</h3>
-                                <div className="remarks_text">
-                                    {excerpt}
-                                </div>
-                            </div>
-
-                            <div className='right'>
-                                <div className='icon'>
-                                    <Link href="/blog/admin/edit" data={{ id: id }}>
-                                        <FaEdit className='icon'/>
-                                    </Link>
-                                    <button>
-                                        <RendarVisibility props={[is_show,id]}/>
-                                    </button>
-                                    <button>
-                                        <FaTrash className='icon' id={id} onClick={(e)=>handleClickDelete(e)}/>
-                                    </button>
-                                </div>
-                                <div className="date">
-                                        <UpdateDate />
-                                </div> 
-                            </div>
-                        </div>
-                    )
-                })}
-                </>
-            )
-        }else{
-            return(
-                <>
-                    {searchState.category?categoryName[Number(searchState.category)]+"の中に､":""}
-                    {searchState.keyword}のキーワードで
-                    ヒットする記事がありません｡
-                </>
-                
-            )
-        }
-              
-        
-     }
-     
-	return (
-        <AuthenticatedLayout user={auth.user}>
-            <Head title="Admin TOP" />
-                <section className="section blog editIndex">
-                    <h1 className="section_title">
-                    <div className="section_title_jp">Admin</div>
-                    </h1>
-                    <ul className="category_tab tab">  
-                        <li className={searchState.category === 1?"category_tab_li on":"category_tab_li"} 
-                        tabIndex="-1" value="1"  
-                        onClick={(e)=>{setSearchState(searchState => ({...searchState, "category":e.target.value}))}}>
-                            {categoryName[1]}
-                        </li>
-                        <li className={searchState.category === 4?"category_tab_li on":"category_tab_li"} 
-                        tabIndex="-1" value="4" 
-                        onClick={(e)=>{setSearchState(searchState => ({...searchState, "category":e.target.value}))}}>
-                            {categoryName[4]}
-                        </li>
-                        <li className={searchState.category === 5?"category_tab_li on":"category_tab_li"} 
-                        tabIndex="-1" value="5" 
-                        onClick={(e)=>{setSearchState(searchState => ({...searchState, "category":e.target.value}))}}>
-                            {categoryName[5]}
-                        </li>
-                    
-                        
-                    </ul>
-                    <div className="search_area js-search_area edit">
-                        <button type="button" className="search_area_reset js-search_area_reset edit" value="RESET" onClick={reset}>
-                            <AiOutlineClear />
-                        </button>
-                        <BsSearch className="search_area_icon js-search_area_icon"/>
-                        <input list="tag-list"  className="search_area_input js-search_area_input edit" id="tag-choice" 
-                            name="tag-choice" placeholder=""  
-                            value={searchState.keyword}
-                            onChange={(e)=>{
-                                setSearchState(searchState => ({...searchState, "keyword":e.target.value}))
-                            }
-                            } 
-                        />
-                        <datalist id="tag-list">
-                                      
-                        </datalist>
-                    </div>
-                    <div className="section_content posts edit">
-                        <RendarallPage />
-                    </div>
-                </section> 
-        </AuthenticatedLayout> 
-	);
-  }
-
-export default function Blog(){
-    
-    return <BlogIndex />;
+    return(
+        <Authenticated>
+            <Head>
+                <title>Admin</title>
+                <meta name="description" content="WEBエンジニアとしてのポートフォリオ､またWEB制作やWEB開発に関わる知識を発信しています" />
+            </Head>
+            <section className="section blog edit">
+                <h1 className="section_title">
+                    <p className="section_title_jp">BLOG ADMIN</p>
+                </h1>
+                <CommonBlog handleClickDelete={handleClickDelete} handleClickVisible={handleClickVisible}/>
+            </section>
+        </Authenticated>
+        );
 }
--- resources/js/Pages/Posts/Index.jsx
+++ resources/js/Pages/Posts/Index.jsx
@@ -1,29 +1,50 @@
-import { usePage, Link, Head} from '@inertiajs/react'
+import { usePage, Link, Head,router} from '@inertiajs/react'
 import React, {useState } from "react";
 import $ from 'jquery';
 import {fixedSearch,formatDate,bg } from "@/Script";
 import GuestLayout from '@/Layouts/GuestLayout';
+import Authenticated from '@/Layouts/AuthenticatedLayout';
 import {BsSearch} from 'react-icons/bs';
 import {MdAccessTime, MdUpdate} from 'react-icons/md';
 import {AiOutlineClear} from 'react-icons/ai';
 import parse from 'html-react-parser';
 
 import {AiOutlineEyeInvisible,AiOutlineEye} from 'react-icons/ai'
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-
-
 import { FaTrash,FaEdit } from 'react-icons/fa';
 
 
 
-export default function BlogIndex() {
+
+export default function GuestBlog(){
+    return(
+        <GuestLayout>
+            <Head>
+                <title>BLOG</title>
+                <meta name="description" content="WEBエンジニアとしてのポートフォリオ､またWEB制作やWEB開発に関わる知識を発信しています" />
+            </Head>
+            <div className='background'>
+                <div className="images">
+                    <img className="flow-image 0" src="/userfiles/images/africa.png" />
+                </div>
+            </div>
+            <section className="section blog">
+                <h1 className="section_title">
+                    <p className="section_title_jp">BLOG</p>
+                </h1>
+                <CommonBlog />
+            </section>
+        </GuestLayout>
+        )
+}
+
+function CommonBlog({handleClickVisible, handleClickDelete}) {
     fixedSearch();
     const loadPosts = usePage().props.loadPosts.data;
     const [posts, setPosts] = useState(loadPosts);
     const [keyword, setKeyword] = useState("");
     const [category, setCategory] = useState("");
     const uri = usePage().component;
-    console.log(uri)
+    
     const handleChangeKeyword = (e)=>{
         const inputKeyword = e.currentTarget.value;
         setKeyword(inputKeyword);
@@ -49,53 +70,38 @@ export default function BlogIndex() {
     }
     const handleClickReset = ()=>{
         $(".search_area_input").val("");
-         setSearchState(searchState => ({...searchState, "keyword":""}))
-         setSearchState(searchState => ({...searchState, "category":""}))
+         setKeyword("");
+         setCategory("");
          setPosts(loadPosts);
          $(".js-search_area_icon").removeClass("fixed");
-        
      }
-        return (
-            <GuestLayout>
-                    <Head>
-                        <title>BLOG</title>
-                        <meta name="description" content="WEBエンジニアとしてのポートフォリオ､またWEB制作やWEB開発に関わる知識を発信しています" />
-                    </Head>
-                    <div className='background'>
-                        <div className="images">
-                            <img className="flow-image 0" src="/userfiles/images/africa.png" />
-                        </div>
-                    </div>
-                    <section className="section blog editIndex">
-                        <h1 className="section_title">
-                        <div className="section_title_jp">BLOG</div>
-                        </h1>
-                        <ShowCategoryTab  hanldeClickCategory={hanldeClickCategory} setCategoryName={setCategoryName} category={category}/>
-                        <div className="search_area js-search_area">
-                            <button type="button" className="search_area_reset js-search_area_reset" value="RESET" 
-                                    onClick={handleClickReset}>
-                                <AiOutlineClear />
-                            </button>
-                            <BsSearch className="search_area_icon js-search_area_icon"/>
-                            
-                            <input list="tag-list"  className="search_area_input js-search_area_input" id="tag-choice" 
-                                name="tag-choice" placeholder=""  
-                                value={keyword}
-                                onChange={handleChangeKeyword} 
-                            />
-                            
-                        </div>
-                        <div className="section_content posts edit">
-                           <ShowPosts posts={posts} category={category} keyword={keyword} uri={uri}/>         
-                        </div>
-                    </section> 
-            </GuestLayout> 
-        );
-    
-	
+    return(
+            <>
+                <ShowCategoryTab  hanldeClickCategory={hanldeClickCategory} setCategoryName={setCategoryName} category={category}/>
+                <div className="search_area js-search_area">
+                    <button type="button" className="search_area_reset js-search_area_reset" value="RESET" 
+                            onClick={handleClickReset}>
+                        <AiOutlineClear />
+                    </button>
+                    <BsSearch className="search_area_icon js-search_area_icon"/>
+                    
+                    <input list="tag-list"  className="search_area_input js-search_area_input" id="tag-choice" 
+                        name="tag-choice" placeholder=""  
+                        value={keyword}
+                        onChange={handleChangeKeyword} 
+                    />
+                </div>
+                <div className="section_content posts edit">
+                   <ShowPosts 
+                        posts={posts} category={category} keyword={keyword} uri={uri}
+                        handleClickVisible={handleClickVisible} handleClickDelete={handleClickDelete}
+                    />         
+                </div>
+            </>
+        )
 }
 
-function ShowPosts({posts, category, keyword, uri}){
+function ShowPosts({posts, category, keyword, uri, handleClickVisible, handleClickDelete}){
     const UpdateDate = ({post})=>{
         if(formatDate(post.published_at) == "1970/01/01"){
           return(
@@ -129,91 +135,73 @@ function ShowPosts({posts, category, keyword, uri}){
         }
     }
     if(uri == "Posts/EditIndex"){
-    const RendarVisibility= (props)=>{
-        const is_show = props.props[0];
-        const id = props.props[1];
-
-        if(is_show){
-            return(
-                <AiOutlineEye className='icon' id={id} alt='表示する' onClick={(e)=>handleClickVisible(e,1)}/>
-            )
-        }else{
-            return(
-                <AiOutlineEyeInvisible className='icon' id={id} alt='非表示にする' onClick={(e)=>handleClickVisible(e,0)}/>
-            )
-        } 
-     }
-    
-    return(
-
-        posts.map((post,i)=>(
-            <div className={post.is_show?'article edit': 'article edit unshown'} id={post.id} key={post.id}>
-                <div className='id'>
-                    {post.id}
-                </div>
-                <div className="remarks">
-                    <h3 className="remarks_title">{post.title}</h3>
-                    <div className="remarks_text">
-                        {post.excerpt}
+        return(
+            posts.map((post,i)=>(
+                <div className={post.is_show?'article edit': 'article edit unshown'} id={post.id} key={post.id}>
+                    <div className='id'>
+                        {post.id}
+                    </div>
+                    <div className="remarks">
+                        <h3 className="remarks_title">{post.title}</h3>
+                        <div className="remarks_text">
+                            {post.excerpt}
+                        </div>
+                    </div>
+                    <div className='right'>
+                        <div className='icon'>
+                            <Link href="/blog/admin/edit" data={{ id: post.id }}>
+                                <FaEdit className='icon'/>
+                            </Link>
+                            <button>
+                                {post.is_show
+                                    ?<AiOutlineEye className='icon' id={post.id} alt='表示する' onClick={(e)=>handleClickVisible(e,1)}/>
+                                    :<AiOutlineEye className='icon' id={post.id} alt='表示する' onClick={(e)=>handleClickVisible(e,0)}/>
+                                 }
+                            </button>
+                            <button>
+                                <FaTrash className='icon' id={post.id} 
+                                  onClick={(e)=>handleClickDelete(e,uri)}/>
+                            </button>
+                        </div>
+                        <div className="date">
+                                <UpdateDate post={post}/>
+                        </div> 
                     </div>
                 </div>
-
-                <div className='right'>
-                    <div className='icon'>
-                        <Link href="/blog/admin/edit" data={{ id: post.id }}>
-                            <FaEdit className='icon'/>
-                        </Link>
-                        <button>
-                            <RendarVisibility props={[post.is_show,post.id]}/>
-                            {console.log(post.is_show)}
-                        </button>
-                        <button>
-                            <FaTrash className='icon' id={post.id} onClick={(e)=>handleClickDelete(e)}/>
-                        </button>
-                    </div>
-                    <div className="date">
-                            <UpdateDate post={post}/>
-                    </div> 
-                </div>
-            </div>
-        
-        ))
-    )
+            ))
+        )
         
     }else{
         if(posts.length >0){
-        return(
-            <>
-                {posts.map((post,i)=> {
-                    return(
-                        <div className="article fade" id={post.id} key={post.id}>
-                            <Link href="/blog/page" data={{ id: post.id }} className='article_link'>
-                                <div className="article_link_img">
-                                    {parse(post.thumbnail)}
-                                </div>
-                                <div className="article_link_remarks">
-                                    <h3 className="article_link_remarks_title">{post.title}</h3>
-                                    <div className="article_link_remarks_text">
-                                        {post.excerpt}
+            return(
+                <>
+                    {posts.map((post,i)=> {
+                        return(
+                            <div className="article fade" id={post.id} key={post.id}>
+                                <Link href="/blog/page" data={{ id: post.id }} className='article_link'>
+                                    <div className="article_link_img">
+                                        {parse(post.thumbnail)}
                                     </div>
-                                    <UpdateDate post={post}/>  
-                                </div>
-                            </Link>
-                        </div>
-                    )
-                })}
-            </>
+                                    <div className="article_link_remarks">
+                                        <h3 className="article_link_remarks_title">{post.title}</h3>
+                                        <div className="article_link_remarks_text">
+                                            {post.excerpt}
+                                        </div>
+                                        <UpdateDate post={post}/>  
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })}
+                </>
             )
-    }else{
-        return(
+        }else{
+            return(
                 <div className='fade'>
                     {category?setCategoryName[Number(category)]+"の中に､":""}
-                    {keyword}のキーワードで
-                    ヒットする記事がありません｡
-                </div>
-                
+                    {keyword}のキーワードでヒットする記事がありません｡
+                </div> 
             )
-
     }
     }
     
@@ -247,4 +235,4 @@ const setCategoryName = [
 ]
   
 
-export {ShowPosts,ShowCategoryTab,setCategoryName};
+export {CommonBlog};
