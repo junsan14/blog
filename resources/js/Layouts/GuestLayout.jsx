874e41f723bb1ecf005efe5ba1960b0b96b96996
junsan14 <jusnanjunsan14@gmail.com>
2024-12-01 14:06:27 +0200

    241201 style edit2

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -35,14 +35,6 @@ function Layout({user,children,status}){
           <span></span>
       </div> 
       <nav className="nav js-nav">
-        <div className="nav_sns">
-            <a href="https://www.threads.net/@junsan_junsan14/" target="_blank" rel="noopener noreferrer">
-              <FaThreads className='nav_sns_icon'/>
-            </a>
-            <a href="https://www.instagram.com/junsan_junsan14" rel="noopener noreferrer" target='_blank'>
-              <FaInstagram className='nav_sns_icon'/>
-            </a>
-        </div>
         <ul className="nav_ul">
            <li className="nav_ul_li js-nav-ul-li"><Link href={route("home")}>HOME</Link></li>
            <li className="nav_ul_li js-nav-ul-li"><Link href={route("about")}>ABOUT</Link></li>
@@ -51,6 +43,14 @@ function Layout({user,children,status}){
            <li className="nav_ul_li js-nav-ul-li"><Link href={route("contact")}>CONTACT</Link></li>
            {is_login&&  <li className="nav_ul_li js-nav-ul-li"><Link href={route("blog.edit")}>管理画面</Link></li>}
         </ul>
+        <div className="nav_sns">
+          <a href="https://www.threads.net/@junsan_junsan14/" target="_blank" rel="noopener noreferrer">
+            <FaThreads className='nav_sns_icon'/>
+          </a>
+          <a href="https://www.instagram.com/junsan_junsan14" rel="noopener noreferrer" target='_blank'>
+            <FaInstagram className='nav_sns_icon'/>
+          </a>
+        </div>
       </nav>
     </header>
     <main className="main guest-wrapper">
